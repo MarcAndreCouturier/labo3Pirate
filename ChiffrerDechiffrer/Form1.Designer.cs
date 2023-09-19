82535c5b63996ae63dbf04d7ce1566d520b3e7ad
Aur3ll <marcandrecouturier1201@gmail.com>
2023-09-18 22:04:48 -0400

    remise

---
+++ ChiffrerDechiffrer/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ChiffrerDechiffrer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.algorithme = new System.Windows.Forms.ComboBox();
+            this.lblAlgoSelection = new System.Windows.Forms.Label();
+            this.cles = new System.Windows.Forms.TextBox();
+            this.texteUtilisateur = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.texteTransforme = new System.Windows.Forms.TextBox();
+            this.chiffrer = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dechiffrer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // algorithme
+            // 
+            this.algorithme.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.algorithme.FormattingEnabled = true;
+            this.algorithme.Items.AddRange(new object[] {
+            "Aucun",
+            "TripleDES",
+            "AES",
+            "DSA",
+            "RSA"});
+            this.algorithme.Location = new System.Drawing.Point(318, 95);
+            this.algorithme.Name = "algorithme";
+            this.algorithme.Size = new System.Drawing.Size(175, 28);
+            this.algorithme.TabIndex = 0;
+            this.algorithme.Text = "Aucun";
+            this.algorithme.SelectedIndexChanged += new System.EventHandler(this.algorithme_SelectedIndexChanged);
+            // 
+            // lblAlgoSelection
+            // 
+            this.lblAlgoSelection.AutoSize = true;
+            this.lblAlgoSelection.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlgoSelection.Location = new System.Drawing.Point(128, 98);
+            this.lblAlgoSelection.Name = "lblAlgoSelection";
+            this.lblAlgoSelection.Size = new System.Drawing.Size(184, 20);
+            this.lblAlgoSelection.TabIndex = 1;
+            this.lblAlgoSelection.Text = "Algorithme de cryptage : ";
+            this.lblAlgoSelection.Click += new System.EventHandler(this.lblAlgoSelection_Click);
+            // 
+            // cles
+            // 
+            this.cles.Location = new System.Drawing.Point(318, 139);
+            this.cles.Name = "cles";
+            this.cles.Size = new System.Drawing.Size(613, 20);
+            this.cles.TabIndex = 2;
+            // 
+            // texteUtilisateur
+            // 
+            this.texteUtilisateur.Location = new System.Drawing.Point(318, 165);
+            this.texteUtilisateur.Multiline = true;
+            this.texteUtilisateur.Name = "texteUtilisateur";
+            this.texteUtilisateur.Size = new System.Drawing.Size(555, 213);
+            this.texteUtilisateur.TabIndex = 3;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.AutoSize = false;
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 26);
+            // 
+            // texteTransforme
+            // 
+            this.texteTransforme.Location = new System.Drawing.Point(318, 385);
+            this.texteTransforme.Multiline = true;
+            this.texteTransforme.Name = "texteTransforme";
+            this.texteTransforme.Size = new System.Drawing.Size(555, 224);
+            this.texteTransforme.TabIndex = 5;
+            // 
+            // chiffrer
+            // 
+            this.chiffrer.Location = new System.Drawing.Point(879, 165);
+            this.chiffrer.Name = "chiffrer";
+            this.chiffrer.Size = new System.Drawing.Size(75, 23);
+            this.chiffrer.TabIndex = 6;
+            this.chiffrer.Text = "chiffrer";
+            this.chiffrer.UseVisualStyleBackColor = true;
+            this.chiffrer.Click += new System.EventHandler(this.chiffrer_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(788, 616);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dechiffrer
+            // 
+            this.dechiffrer.Location = new System.Drawing.Point(879, 205);
+            this.dechiffrer.Name = "dechiffrer";
+            this.dechiffrer.Size = new System.Drawing.Size(75, 23);
+            this.dechiffrer.TabIndex = 8;
+            this.dechiffrer.Text = "dechiffrer";
+            this.dechiffrer.UseVisualStyleBackColor = true;
+            this.dechiffrer.Click += new System.EventHandler(this.dechiffrer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 712);
+            this.Controls.Add(this.dechiffrer);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chiffrer);
+            this.Controls.Add(this.texteTransforme);
+            this.Controls.Add(this.texteUtilisateur);
+            this.Controls.Add(this.cles);
+            this.Controls.Add(this.lblAlgoSelection);
+            this.Controls.Add(this.algorithme);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox algorithme;
+        private System.Windows.Forms.Label lblAlgoSelection;
+        private System.Windows.Forms.TextBox cles;
+        private System.Windows.Forms.TextBox texteUtilisateur;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox texteTransforme;
+        private System.Windows.Forms.Button chiffrer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button dechiffrer;
+    }
+}
+
---
+++ ChiffrerDechiffrer/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChiffrerDechiffrer
+{
+    /* pour la question sur le vecteur d'initialisation de ce que je comprends la clef génère un key stream fixe pour une clef x. 
+     * le key stream sert a changer les bytes du  message avec un xor entre les bytes du key  stream et du message et le vecteur
+     * d'initialisation sert a modifier périodiquement le key stream pour éviter que quelqu'un puisse faire de l'ingénérie
+     * inverse sur le keystream à l'aide de répétition prévisible dans le message. 
+     */
+
+    /*
+     * pour la question 5  on ne pourrait pas craker le message avec la meme clef, car ils utilisent un algorithme différent pour
+     * générer le key stream ce qui fait que le xor entre les bytes donnerait un résultat différent.
+     */
+    public partial class Form1 : Form
+    {
+        public enum NomAlgorithme
+        {
+            Aucun, TripleDES, AES, DSA, RSA
+        }
+
+        Object algo { get; set; } = null; // Permet de conserver la clé intacte entre les opérations
+        byte[] hash { get; set; } = null; // Utile pour l'algorithme DSA
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void algorithme_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if ((string)this.algorithme.SelectedItem == NomAlgorithme.Aucun.ToString())
+            {
+                // On vide le contenu du champ cles
+                this.cles.Text = "";
+                algo = null;
+            }
+            else if ((string)this.algorithme.SelectedItem == NomAlgorithme.TripleDES.ToString())
+            {
+                TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+                tdes.GenerateKey();
+                tdes.GenerateIV(); // On génère aussi un vecteur d'initialisation
+                this.cles.Text = BitConverter.ToString(tdes.Key) + "   (" + tdes.KeySize + " bits)";
+               
+                algo = tdes;
+            }
+            else if ((string)this.algorithme.SelectedItem == NomAlgorithme.AES.ToString())
+            {
+                RijndaelManaged aes = new RijndaelManaged();
+                aes.GenerateKey();
+                aes.GenerateIV();
+                this.cles.Text = BitConverter.ToString(aes.Key) + "   (" + aes.KeySize + " bits)";
+
+                algo= aes;
+
+            }
+            else if ((string)this.algorithme.SelectedItem == NomAlgorithme.DSA.ToString())
+            {
+                DSACryptoServiceProvider dsa = new DSACryptoServiceProvider(1024);
+                this.cles.Text = dsa.ToXmlString(true) + "    (" + dsa.KeySize + " bits)";
+                dsa.ExportParameters(true);
+                algo = dsa;
+            }
+            else if ((string)this.algorithme.SelectedItem == NomAlgorithme.RSA.ToString())
+            {
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                this.cles.Text = rsa.ToXmlString(true) + "    (" + rsa.KeySize + " bits)";
+                algo = rsa;
+              
+                // Coder ici : déterminer et afficher la clé pour cet algorithme
+            }
+        }
+        private void lblAlgoSelection_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.cles.ReadOnly = true;
+        }
+
+        private void chiffrer_Click(object sender, EventArgs e)
+        {
+            if ((string)this.algorithme.SelectedItem == NomAlgorithme.Aucun.ToString())
+            {
+                // Puisqu'aucun algorithme n'est choisi, on conserve le texte de l'utilisateur en sortie
+                this.texteTransforme.Text = this.texteUtilisateur.Text;
+                return;
+            }
+
+            byte[] donneesBrutes = UTF8Encoding.UTF8.GetBytes(this.texteUtilisateur.Text);
+            byte[] donneesTransformees = null;
+
+            if ((string)this.algorithme.SelectedItem == NomAlgorithme.TripleDES.ToString())
+            {
+                TripleDESCryptoServiceProvider tdes = (TripleDESCryptoServiceProvider)algo;
+                ICryptoTransform encrypteur = tdes.CreateEncryptor(tdes.Key, tdes.IV);
+                donneesTransformees = encrypteur.TransformFinalBlock(donneesBrutes, 0, donneesBrutes.Length);
+                // Coder ici : chiffrer les données pour cet algorithme
+            }
+
+            else if ((string)this.algorithme.SelectedItem == NomAlgorithme.AES.ToString())
+            {
+                RijndaelManaged aes = (RijndaelManaged)algo;
+                ICryptoTransform encrypteur = aes.CreateEncryptor(aes.Key, aes.IV);
+                donneesTransformees = encrypteur.TransformFinalBlock(donneesBrutes, 0, donneesBrutes.Length);
+            }
+            else if ((string)this.algorithme.SelectedItem == NomAlgorithme.DSA.ToString())
+            {
+               DSACryptoServiceProvider dsa = (DSACryptoServiceProvider)algo;
+                donneesTransformees = dsa.CreateSignature(donneesBrutes);
+               
+                return;
+                // Coder ici : produire la signature pour cet algorithme
+            }
+            else if ((string)this.algorithme.SelectedItem == NomAlgorithme.RSA.ToString())
+            {
+                RSACryptoServiceProvider rsa = (RSACryptoServiceProvider)algo;
+                donneesTransformees = rsa.Encrypt(donneesBrutes, true);
+            }
+
+            this.texteTransforme.Text = Convert.ToBase64String(donneesTransformees, 0, donneesTransformees.Length);
+        }
+
+        private void dechiffrer_Click(object sender, EventArgs e)
+        {
+            if ((string)this.algorithme.SelectedItem == NomAlgorithme.Aucun.ToString())
+            {
+                // Puisqu'aucun algorithme n'est choisi, on conserve le texte de l'utilisateur en sortie
+                this.texteTransforme.Text = this.texteUtilisateur.Text;
+                return;
+            }
+
+            byte[] donneesBrutes = Convert.FromBase64String(this.texteUtilisateur.Text);
+            byte[] donneesTransformees = null;
+
+            if ((string)this.algorithme.SelectedItem == NomAlgorithme.TripleDES.ToString())
+            {
+                TripleDESCryptoServiceProvider tdes = (TripleDESCryptoServiceProvider)algo;
+                ICryptoTransform decrypteur = tdes.CreateDecryptor(tdes.Key, tdes.IV);
+                donneesTransformees = decrypteur.TransformFinalBlock(donneesBrutes, 0, donneesBrutes.Length);
+            }
+            else if ((string)this.algorithme.SelectedItem == NomAlgorithme.AES.ToString())
+            {
+                RijndaelManaged aes = (RijndaelManaged)algo;
+                ICryptoTransform decrypteur = aes.CreateDecryptor(aes.Key, aes.IV);
+                donneesTransformees = decrypteur.TransformFinalBlock(donneesBrutes, 0, donneesBrutes.Length);
+            }
+            else if ((string)this.algorithme.SelectedItem == NomAlgorithme.DSA.ToString())
+            {
+                DSACryptoServiceProvider dsa = (DSACryptoServiceProvider)algo;
+                this.texteTransforme.Text = (dsa.VerifySignature(hash, donneesBrutes) ? "Signature valide !" : "Signature invalide !");
+                return;
+            }
+
+            else if ((string)this.algorithme.SelectedItem == NomAlgorithme.RSA.ToString())
+            {
+                RSACryptoServiceProvider rsa = (RSACryptoServiceProvider)algo;
+                donneesTransformees = rsa.Decrypt(donneesBrutes, true);
+                
+            }
+
+            this.texteTransforme.Text = UTF8Encoding.UTF8.GetString(donneesTransformees);
+        }
+    }
+}
